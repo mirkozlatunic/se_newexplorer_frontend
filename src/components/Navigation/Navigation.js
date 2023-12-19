189da614ea1cdfb0ab27b70b1a64d136dc592546
Mirko Zlatunic <mirkozlatunic@Mirkos-MBP.home>
2023-12-18 21:42:12 -0800

    hamburger menu issues

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Navigation.css";
 import { Link, useLocation } from "react-router-dom";
 import { CurrentUserContext } from "../../context/CurrentUserContext";
@@ -10,9 +10,32 @@ const Navigation = ({ isLoggedIn, onSignInModal, onLogout }) => {
   const currentLocation = reactLocation.pathname;
   const currentUser = useContext(CurrentUserContext);
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileMenuOpen(false);
+    };
+
+    // Set initial state
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
-      <header className="navigation">
+      <header className={`navigation ${isMobileMenuOpen ? "menu-open" : ""}`}>
         <Link to="/" className="navigation__link">
           <div
             className={
@@ -25,59 +48,74 @@ const Navigation = ({ isLoggedIn, onSignInModal, onLogout }) => {
           </div>
         </Link>
         <div className="navigation__rightside">
-          <Link to="/" className="navigation__link-home">
+          <div
+            className="navigation__hamburger-menu"
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? "✕" : "☰"}
+          </div>
+          {isMobileMenuOpen && (
+            // Render hamburger menu for mobile
+            // Add onClick handler to toggle mobile menu state
             <div
-              className={
-                currentLocation === "/"
-                  ? "navigation__button navigation__link-home-active"
-                  : "navigation__link-home-saved-news"
-              }
+              className="mobile-menu"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
-              Home
-            </div>
-          </Link>
-          {isLoggedIn && (
-            <Link to="/saved-news" className="navigation__link-saved-news">
-              <div
-                className={
-                  currentLocation === "/saved-news"
-                    ? "navigation__button-saved-news navigation__link-saved-news-active"
-                    : "navigation__button"
-                }
-              >
-                Saved articles
-              </div>
-            </Link>
-          )}
-          {isLoggedIn ? (
-            <button
-              className={
-                currentLocation === "/"
-                  ? "navigation__logout-button"
-                  : "navigation__logout-button-saved-news"
-              }
-              onClick={onLogout}
-            >
-              {currentUser ? currentUser.name : ""}Elise
-              <img
-                src={
-                  currentLocation === "/saved-news"
-                    ? LogoutButton
-                    : LogoutButtonWhite
-                }
-                alt="logout"
-                className="navigation__button-logout"
-              />
-            </button>
-          ) : (
-            <div>
-              <button
-                className="navigation__reg-button"
-                type="text"
-                onClick={onSignInModal}
-              >
-                Sign In
-              </button>
+              <Link to="/" className="navigation__link-home">
+                <div
+                  className={
+                    currentLocation === "/"
+                      ? "navigation__button navigation__link-home-active"
+                      : "navigation__link-home-saved-news"
+                  }
+                >
+                  Home
+                </div>
+              </Link>
+              {isLoggedIn && (
+                <Link to="/saved-news" className="navigation__link-saved-news">
+                  <div
+                    className={
+                      currentLocation === "/saved-news"
+                        ? "navigation__button-saved-news navigation__link-saved-news-active"
+                        : "navigation__button"
+                    }
+                  >
+                    Saved articles
+                  </div>
+                </Link>
+              )}
+              {isLoggedIn ? (
+                <button
+                  className={
+                    currentLocation === "/"
+                      ? "navigation__logout-button"
+                      : "navigation__logout-button-saved-news"
+                  }
+                  onClick={onLogout}
+                >
+                  {currentUser ? currentUser.name : ""}Elise
+                  <img
+                    src={
+                      currentLocation === "/saved-news"
+                        ? LogoutButton
+                        : LogoutButtonWhite
+                    }
+                    alt="logout"
+                    className="navigation__button-logout"
+                  />
+                </button>
+              ) : (
+                <div>
+                  <button
+                    className="navigation__reg-button"
+                    type="text"
+                    onClick={onSignInModal}
+                  >
+                    Sign In
+                  </button>
+                </div>
+              )}
             </div>
           )}
         </div>
